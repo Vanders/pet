55e189834e6524197bd2c5291ce93ccdb187dadd
Kristian Van Der Vliet <vanders@liqwyd.com>
2022-10-30 17:58:25 +0100

    Test LSR & ROR

--- mos6502/t_shift_test.go
+++ mos6502/t_shift_test.go
@@ -114,6 +114,64 @@ func Test_op_asl(t *testing.T) {
 	}.Run(t)
 }
 
+func Test_op_lsr(t *testing.T) {
+	//
+	//	INS_LSR_AC
+	//	INS_LSR_ZP
+	//	INS_LSR_ZPX
+	//
+	testCases{
+		testCase{
+			INS_LSR_AC,
+			"accumulator (no carry)",
+			// Setup
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				c.Registers.A.Set(0x7e)
+			},
+			// Check
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				CompareA(t, c, 0x3f)
+
+				CClear(t, c)
+				NClear(t, c)
+				ZClear(t, c)
+			},
+		},
+		testCase{
+			INS_LSR_AC,
+			"accumulator (carry out)",
+			// Setup
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				c.Registers.A.Set(0xff)
+			},
+			// Check
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				CompareA(t, c, 0x7f)
+
+				CSet(t, c)
+				NClear(t, c)
+				ZClear(t, c)
+			},
+		},
+		testCase{
+			INS_LSR_AC,
+			"accumulator (zero)",
+			// Setup
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				c.Registers.A.Set(0x00)
+			},
+			// Check
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				CompareA(t, c, 0x00)
+
+				CClear(t, c)
+				NClear(t, c)
+				ZSet(t, c)
+			},
+		},
+	}.Run(t)
+}
+
 func Test_op_rol(t *testing.T) {
 	//
 	//	INS_ROL_AC
@@ -212,6 +270,119 @@ func Test_op_rol(t *testing.T) {
 			// Check
 			func(t *testing.T, c *CPU, m *fakeMem) {
 				CompareMem(t, m, 0x01, 0x7e)
+
+				CClear(t, c)
+				NClear(t, c)
+				ZClear(t, c)
+			},
+		},
+	}.Run(t)
+}
+
+func Test_op_ror(t *testing.T) {
+	//
+	//	INS_ROR_AC
+	//	INS_ROR_ZP
+	//	INS_ROR_ZPX
+	//
+	testCases{
+		testCase{
+			INS_ROR_AC,
+			"accumulator (positive, no carry in, no carry out)",
+			// Setup
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				c.Registers.A.Set(0x7e)
+			},
+			// Check
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				CompareA(t, c, 0x3f)
+
+				CClear(t, c)
+				NClear(t, c)
+				ZClear(t, c)
+			},
+		},
+
+		testCase{
+			INS_ROR_AC,
+			"accumulator (negative, carry in, no carry out)",
+			// Setup
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				c.Registers.P.C = true
+				c.Registers.A.Set(0xfe)
+			},
+			// Check
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				CompareA(t, c, 0xff)
+
+				CClear(t, c)
+				NSet(t, c)
+				ZClear(t, c)
+			},
+		},
+		testCase{
+			INS_ROR_AC,
+			"accumulator (negative, carry in, carry out)",
+			// Setup
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				c.Registers.P.C = true
+				c.Registers.A.Set(0x7f)
+			},
+			// Check
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				CompareA(t, c, 0xbf)
+
+				CSet(t, c)
+				NSet(t, c)
+				ZClear(t, c)
+			},
+		},
+		testCase{
+			INS_ROR_AC,
+			"accumulator (zero, no carry out)",
+			// Setup
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				c.Registers.A.Set(0x00)
+			},
+			// Check
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				CompareA(t, c, 0x00)
+
+				CClear(t, c)
+				NClear(t, c)
+				ZSet(t, c)
+			},
+		},
+		testCase{
+			INS_ROR_ZP,
+			"zero page",
+			// Setup
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				m.SetByte(0x01, 0x7e) // ZP $01=$7a
+				m.WriteByte(0x01)
+			},
+			// Check
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				CompareMem(t, m, 0x01, 0x3f)
+
+				CClear(t, c)
+				NClear(t, c)
+				ZClear(t, c)
+			},
+		},
+		testCase{
+			INS_ROR_ZPX,
+			"zero page, indexed x",
+			// Setup
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				m.SetByte(0x02, 0x7e) // ZP $02=$7e
+
+				c.Registers.X.Set(0x01)
+				m.WriteByte(0x01) // $01 + X($01) = ZP $02
+			},
+			// Check
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				CompareMem(t, m, 0x02, 0x3f)
 
 				CClear(t, c)
 				NClear(t, c)
