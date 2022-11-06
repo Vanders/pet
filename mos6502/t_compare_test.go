f4765d60ca9ee1d08454acea50b4ffdc4335216b
Kristian Van Der Vliet <vanders@liqwyd.com>
2022-11-06 16:53:39 +0100

    Add basic tests for CMP
    Test the three main conditions for CMP (absolute)

---
+++ mos6502/t_compare_test.go
@@ -0,0 +1,69 @@
+package mos6502
+
+import (
+	"testing"
+)
+
+func Test_op_cmp(t *testing.T) {
+	//
+	//	INS_CMP_AB
+	//	INS_CMP_IM
+	//	INS_CMP_ZP
+	//	INS_CMP_IY
+	//	INS_CMP_ABY
+	//	INS_CMP_ABX
+	//
+	testCases{
+		testCase{
+			INS_CMP_AB,
+			"absolute (less than)",
+			// Setup
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				c.Registers.A.Set(0x7f)
+				m.WriteByte(0x01)
+			},
+			// Check
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				CompareA(t, c, 0x7f)
+
+				CClear(t, c)
+				NClear(t, c)
+				ZClear(t, c)
+			},
+		},
+		testCase{
+			INS_CMP_AB,
+			"absolute (equals)",
+			// Setup
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				c.Registers.A.Set(0x7f)
+				m.WriteByte(0x7f)
+			},
+			// Check
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				CompareA(t, c, 0x7f)
+
+				CSet(t, c)
+				NClear(t, c)
+				ZSet(t, c)
+			},
+		},
+		testCase{
+			INS_CMP_AB,
+			"absolute (more than)",
+			// Setup
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				c.Registers.A.Set(0x01)
+				m.WriteByte(0x7f)
+			},
+			// Check
+			func(t *testing.T, c *CPU, m *fakeMem) {
+				CompareA(t, c, 0x01)
+
+				CSet(t, c)
+				NClear(t, c)
+				ZClear(t, c)
+			},
+		},
+	}.Run(t)
+}
