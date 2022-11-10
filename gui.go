d42e547272ebad8a26c428016d178c46a2511ec2
Kristian Van Der Vliet <vanders@liqwyd.com>
2022-11-10 17:45:52 +0100

    Completely refactor keyboard handling
    Replace the overly complicated rune mapping with SDL->C= scancode, and
    then have the keyboard convert *that* into a row/column. This means that
    all of the normal keys work and things like shift are handled, but has
    the current downside that any shifted keys on the input (E.g. !) can not
    be typed, as that will be read as a shifted 1 which is a graphics
    character on the PET keyboard.

--- gui.go
+++ gui.go
@@ -12,6 +12,104 @@ const (
 	windowWidth  = 360
 )
 
+var scancodes = map[sdl.Keycode]Byte{
+	/* row 9 */
+	sdl.K_EQUALS:      0x3d,
+	sdl.K_PERIOD:      0x2e,
+	sdl.K_ESCAPE:      0x03,
+	sdl.K_LESS:        0x3c,
+	sdl.K_SPACE:       0x20,
+	sdl.K_LEFTBRACKET: 0x5b,
+	sdl.K_LCTRL:       0x12, // Reverse
+
+	/* row 8 */
+	sdl.K_MINUS:        0x2d,
+	sdl.K_0:            0x30,
+	sdl.K_RSHIFT:       0x00,
+	sdl.K_GREATER:      0x3e,
+	sdl.K_RIGHTBRACKET: 0x5d,
+	sdl.K_AT:           0x40,
+	// We're going to need a non-mapped shift key for shifted input characters
+	// sdl.K_LSHIFT:       0x00,
+
+	/* row 7 */
+	sdl.K_PLUS:     0x2b,
+	sdl.K_2:        0x32,
+	sdl.K_QUESTION: 0x3f,
+	sdl.K_COMMA:    0x2c,
+	sdl.K_n:        0x4e,
+	sdl.K_v:        0x56,
+	sdl.K_x:        0x58,
+
+	/* row 6 */
+	sdl.K_3:         0x33,
+	sdl.K_1:         0x31,
+	sdl.K_RETURN:    0x0d,
+	sdl.K_SEMICOLON: 0x3b,
+	sdl.K_m:         0x4d,
+	sdl.K_b:         0x42,
+	sdl.K_c:         0x43,
+	sdl.K_z:         0x5a,
+
+	/* row 5 */
+	sdl.K_ASTERISK: 0x2a, // *
+	sdl.K_5:        0x35,
+	sdl.K_COLON:    0x3a,
+	sdl.K_k:        0x4b,
+	sdl.K_h:        0x48,
+	sdl.K_f:        0x46,
+	sdl.K_s:        0x53,
+
+	/* row 4 */
+	sdl.K_6: 0x36,
+	sdl.K_4: 0x34,
+	sdl.K_l: 0x4c,
+	sdl.K_j: 0x4a,
+	sdl.K_g: 0x47,
+	sdl.K_d: 0x44,
+	sdl.K_a: 0x41,
+
+	/* row 3 */
+	sdl.K_SLASH: 0x2f,
+	sdl.K_8:     0x38,
+	sdl.K_p:     0x50,
+	sdl.K_i:     0x49,
+	sdl.K_y:     0x59,
+	sdl.K_r:     0x52,
+	sdl.K_w:     0x57,
+
+	/* row 2 */
+	sdl.K_9:     0x39,
+	sdl.K_7:     0x37,
+	sdl.K_CARET: 0x5e, // ^
+	sdl.K_o:     0x4f,
+	sdl.K_u:     0x55,
+	sdl.K_t:     0x54,
+	sdl.K_e:     0x45,
+	sdl.K_q:     0x51,
+
+	/* row 1 */
+	sdl.K_DELETE:     0x14,
+	sdl.K_UP:         0x11, // UP & DOWN are the same key, shifted
+	sdl.K_DOWN:       0x11,
+	sdl.K_RIGHTPAREN: 0x29, // )
+	sdl.K_BACKSLASH:  0x5c,
+	sdl.K_QUOTE:      0x27, // '
+	sdl.K_DOLLAR:     0x24,
+	sdl.K_QUOTEDBL:   0x22, // "
+
+	/* row 0 */
+	sdl.K_LEFT:      0x1d, // LEFT & RIGHT are the same key, shifted
+	sdl.K_RIGHT:     0x1d,
+	sdl.K_HOME:      0x13,
+	sdl.K_BACKSPACE: 0x14,
+	sdl.K_LEFTPAREN: 0x28, // (
+	sdl.K_AMPERSAND: 0x26, // &
+	sdl.K_PERCENT:   0x25, // %
+	sdl.K_HASH:      0x23, // #
+	sdl.K_EXCLAIM:   0x21, // !
+}
+
 type GUI struct {
 	Video *Video
 
@@ -71,16 +169,6 @@ func (g *GUI) EventLoop(ctx context.Context, events chan<- Event) {
 	lastTicks := sdl.GetTicks()
 	currentTicks := lastTicks
 
-	keymap := make(map[sdl.Keycode]int)
-	keymap[sdl.K_DOWN] = 0x11
-	keymap[sdl.K_LEFT] = 0x1d
-	keymap[sdl.K_UP] = 0x11
-	keymap[sdl.K_RIGHT] = 0x1d
-	keymap[sdl.K_LSHIFT] = 0x00
-	keymap[sdl.K_RSHIFT] = 0x00
-	keymap[sdl.K_HOME] = 0x13
-	keymap[sdl.K_LCTRL] = 0x12 // Reverse
-
 	for {
 		select {
 		case <-ctx.Done():
@@ -91,84 +179,33 @@ func (g *GUI) EventLoop(ctx context.Context, events chan<- Event) {
 
 		sdlEvent := sdl.PollEvent()
 		switch event := sdlEvent.(type) {
-		case *sdl.TextInputEvent:
-			char := rune(event.Text[0])
-
-			// If this key isn't the same as before, send a KEY_UP event for the previous key
-			if g.lastChar != char {
-				events <- EventKeypress{
-					Key: Keypress{
-						Char:  g.lastChar,
-						State: KEY_UP,
-					},
-				}
-			}
-
-			// Remember what was typed for subsequent keypress events
-			g.lastChar = char
-
-			// Send a KEY_DOWN event for this printable key
-			events <- EventKeypress{
-				Key: Keypress{
-					Char:  g.lastChar,
-					State: KEY_DOWN,
-				},
-			}
 		case *sdl.KeyboardEvent:
+			sym := event.Keysym.Sym
+			scancode, ok := scancodes[sym]
+			if !ok {
+				break
+			}
 			k := Keypress{
-				Keycode: int(event.Keysym.Sym),
+				Scancode: scancode,
+			}
+			if event.State == sdl.PRESSED {
+				k.State = KEY_DOWN
+			} else if event.State == sdl.RELEASED {
+				k.State = KEY_UP
 			}
 
-			// Remap certain special keys
-			sym := event.Keysym.Sym
+			// Some keys are shifted and we need to make that explicit
 			switch sym {
-			case sdl.K_DOWN,
-				sdl.K_RIGHT,
-				sdl.K_HOME,
-				sdl.K_LCTRL:
-				k.Keycode = keymap[sym]
 			case sdl.K_UP,
 				sdl.K_LEFT,
 				sdl.K_LSHIFT,
 				sdl.K_RSHIFT:
-				k.Keycode = keymap[sym]
-				k.Modifiers.Shift = true
+				k.Shifted = true
 			}
 
-			// Only send KEY_DOWN events for non-printable keys that are interesting
-			if event.State == sdl.PRESSED {
-				k.State = KEY_DOWN
-
-				switch sym {
-				case sdl.K_RETURN,
-					sdl.K_ESCAPE,
-					sdl.K_BACKSPACE,
-					sdl.K_LSHIFT,
-					sdl.K_RSHIFT,
-					sdl.K_DOWN,
-					sdl.K_RIGHT,
-					sdl.K_UP,
-					sdl.K_LEFT:
-
-					// Send new key press event
-					events <- EventKeypress{
-						Key: k,
-					}
-				default:
-					// Ignore everything else; TextInputEvent will send KEY_DOWN events for those
-					break
-				}
-			} else if event.State == sdl.RELEASED {
-				k.State = KEY_UP
-
-				// Send the last printable character that was typed with this KEY_UP and reset the key
-				k.Char = g.lastChar
-				g.lastChar = rune(0)
-
-				// Send new key press event
-				events <- EventKeypress{
-					Key: k,
-				}
+			// Send new key press event
+			events <- EventKeypress{
+				Key: k,
 			}
 		case *sdl.QuitEvent:
 			events <- EventQuit{}
--- keyboard.go
+++ keyboard.go
@@ -2,12 +2,9 @@ package main
 
 // Keypress contains the data from a key press
 type Keypress struct {
-	Keycode   int
-	Char      rune
-	State     KeyState
-	Modifiers struct {
-		Shift bool
-	}
+	Scancode Byte
+	State    KeyState
+	Shifted  bool
 }
 
 type KeyState int
@@ -57,129 +54,119 @@ type Keyboard struct {
 	Buffer chan<- (Key) // Keyboard "buffer"
 
 	matrix Matrix // Keyboard scan matrix
-	keys   map[rune]Key
+	keys   map[Byte]Key
 }
 
 func (kbd *Keyboard) Reset() {
 	kbd.matrix.Reset()
 
-	kbd.keys = make(map[rune]Key)
-	kbd.keys['='] = Key{9, 7}
-	kbd.keys['.'] = Key{9, 6}
-	//kbd.keys[''] = Key{9, 5}
-	kbd.keys['\033'] = Key{9, 4} // ^C/STOP (Escape)
-	kbd.keys['<'] = Key{9, 3}
-	kbd.keys[' '] = Key{9, 2}
-	kbd.keys['['] = Key{9, 1}
+	kbd.keys = make(map[Byte]Key)
+	kbd.keys[0x3d] = Key{9, 7}
+	kbd.keys[0x2e] = Key{9, 6}
+	kbd.keys[0xff] = Key{9, 5} // UNUSED
+	kbd.keys[0x03] = Key{9, 4} // ^C/STOP (Escape)
+	kbd.keys[0x3c] = Key{9, 3}
+	kbd.keys[0x20] = Key{9, 2}
+	kbd.keys[0x5b] = Key{9, 1}
 	kbd.keys[0x12] = Key{9, 0} // ^R/REVERSE ON
 
-	kbd.keys['-'] = Key{8, 7}
-	kbd.keys['0'] = Key{8, 6}
+	kbd.keys[0x2d] = Key{8, 7}
+	kbd.keys[0x30] = Key{8, 6}
 	kbd.keys[0x00] = Key{8, 5} // RIGHT SHIFT
-	kbd.keys['>'] = Key{8, 4}
-	//kbd.keys[''] = Key{8,3}
-	kbd.keys[']'] = Key{8, 2}
-	kbd.keys['@'] = Key{8, 1}
+	kbd.keys[0x3e] = Key{8, 4}
+	kbd.keys[0xff] = Key{8, 3} // UNUSED
+	kbd.keys[0x5d] = Key{8, 2}
+	kbd.keys[0x40] = Key{8, 1}
 	kbd.keys[0x00] = Key{8, 0} // LEFT SHIFT
 
-	kbd.keys['+'] = Key{7, 7}
-	kbd.keys['2'] = Key{7, 6}
-	//kbd.keys[''] = Key{7,5}
-	kbd.keys['?'] = Key{7, 4}
-	kbd.keys[','] = Key{7, 3}
-	kbd.keys['n'] = Key{7, 2}
-	kbd.keys['v'] = Key{7, 1}
-	kbd.keys['x'] = Key{7, 0}
+	kbd.keys[0x2b] = Key{7, 7}
+	kbd.keys[0x32] = Key{7, 6}
+	kbd.keys[0xff] = Key{7, 5} // UNUSED
+	kbd.keys[0x3f] = Key{7, 4}
+	kbd.keys[0x2c] = Key{7, 3}
+	kbd.keys[0x4e] = Key{7, 2}
+	kbd.keys[0x56] = Key{7, 1}
+	kbd.keys[0x58] = Key{7, 0}
 
-	kbd.keys['3'] = Key{6, 7}
-	kbd.keys['1'] = Key{6, 6}
-	kbd.keys['\r'] = Key{6, 5} //RETURN
+	kbd.keys[0x33] = Key{6, 7}
+	kbd.keys[0x31] = Key{6, 6}
+	kbd.keys[0x0d] = Key{6, 5} // RETURN
+	kbd.keys[0x3b] = Key{6, 4}
+	kbd.keys[0x4d] = Key{6, 3}
+	kbd.keys[0x42] = Key{6, 2}
+	kbd.keys[0x43] = Key{6, 1}
+	kbd.keys[0x5a] = Key{6, 0}
 
-	kbd.keys[';'] = Key{6, 4}
-	kbd.keys['m'] = Key{6, 3}
-	kbd.keys['b'] = Key{6, 2}
-	kbd.keys['c'] = Key{6, 1}
-	kbd.keys['z'] = Key{6, 0}
+	kbd.keys[0x2a] = Key{5, 7}
+	kbd.keys[0x35] = Key{5, 6}
+	kbd.keys[0xff] = Key{5, 5} // UNUSED
+	kbd.keys[0x3a] = Key{5, 4}
+	kbd.keys[0x4b] = Key{5, 3}
+	kbd.keys[0x48] = Key{5, 2}
+	kbd.keys[0x46] = Key{5, 1}
+	kbd.keys[0x53] = Key{5, 0}
 
-	kbd.keys['*'] = Key{5, 7}
-	kbd.keys['5'] = Key{5, 6}
-	//kbd.keys[''] = Key{5,5}
-	kbd.keys[':'] = Key{5, 4}
-	kbd.keys['k'] = Key{5, 3}
-	kbd.keys['h'] = Key{5, 2}
-	kbd.keys['f'] = Key{5, 1}
-	kbd.keys['s'] = Key{5, 0}
+	kbd.keys[0x36] = Key{4, 7}
+	kbd.keys[0x34] = Key{4, 6}
+	kbd.keys[0xff] = Key{4, 5} // UNUSED
+	kbd.keys[0x4c] = Key{4, 4}
+	kbd.keys[0x4a] = Key{4, 3}
+	kbd.keys[0x47] = Key{4, 2}
+	kbd.keys[0x44] = Key{4, 1}
+	kbd.keys[0x41] = Key{4, 0}
 
-	kbd.keys['6'] = Key{4, 7}
-	kbd.keys['4'] = Key{4, 6}
-	//kbd.keys[''] = Key{4,5}
-	kbd.keys['l'] = Key{4, 4}
-	kbd.keys['j'] = Key{4, 3}
-	kbd.keys['g'] = Key{4, 2}
-	kbd.keys['d'] = Key{4, 1}
-	kbd.keys['a'] = Key{4, 0}
-	kbd.keys['A'] = Key{4, 0}
+	kbd.keys[0x2f] = Key{3, 7}
+	kbd.keys[0x38] = Key{3, 6}
+	kbd.keys[0xff] = Key{3, 5} // UNUSED
+	kbd.keys[0x50] = Key{3, 4}
+	kbd.keys[0x49] = Key{3, 3}
+	kbd.keys[0x59] = Key{3, 2}
+	kbd.keys[0x52] = Key{3, 1}
+	kbd.keys[0x57] = Key{3, 0}
 
-	kbd.keys['/'] = Key{3, 7}
-	kbd.keys['8'] = Key{3, 6}
-	//kbd.keys[''] = Key{3,5}
-	kbd.keys['p'] = Key{3, 4}
-	kbd.keys['i'] = Key{3, 3}
-	kbd.keys['y'] = Key{3, 2}
-	kbd.keys['r'] = Key{3, 1}
-	kbd.keys['w'] = Key{3, 0}
+	kbd.keys[0x39] = Key{2, 7}
+	kbd.keys[0x37] = Key{2, 6}
+	kbd.keys[0x5e] = Key{2, 5}
+	kbd.keys[0x4f] = Key{2, 4}
+	kbd.keys[0x55] = Key{2, 3}
+	kbd.keys[0x54] = Key{2, 2}
+	kbd.keys[0x45] = Key{2, 1}
+	kbd.keys[0x51] = Key{2, 0}
 
-	kbd.keys['9'] = Key{2, 7}
-	kbd.keys['7'] = Key{2, 6}
-	kbd.keys['^'] = Key{2, 5}
-	kbd.keys['o'] = Key{2, 4}
-	kbd.keys['u'] = Key{2, 3}
-	kbd.keys['t'] = Key{2, 2}
-	kbd.keys['e'] = Key{2, 1}
-	kbd.keys['q'] = Key{2, 0}
-
-	kbd.keys[0x08] = Key{1, 7} // DEL
+	kbd.keys[0x14] = Key{1, 7} // DEL
 	kbd.keys[0x11] = Key{1, 6} // cursor down
-	//kbd.keys[''] = Key{1,5}
-	kbd.keys[')'] = Key{1, 4}
-	kbd.keys['\\'] = Key{1, 3}
-	kbd.keys['\''] = Key{1, 2}
-	kbd.keys['$'] = Key{1, 1}
-	kbd.keys['"'] = Key{1, 0}
+	kbd.keys[0xff] = Key{1, 5} // UNUSED
+	kbd.keys[0x29] = Key{1, 4}
+	kbd.keys[0x5c] = Key{1, 3}
+	kbd.keys[0x27] = Key{1, 2}
+	kbd.keys[0x24] = Key{1, 1}
+	kbd.keys[0x22] = Key{1, 0}
 
 	kbd.keys[0x1d] = Key{0, 7} // cursor right
 	kbd.keys[0x13] = Key{0, 6} // home
-	//kbd.keys['\177'] = Key{0, 5} // backspace
-	kbd.keys['('] = Key{0, 4}
-	kbd.keys['&'] = Key{0, 3}
-	kbd.keys['%'] = Key{0, 2}
-	kbd.keys['#'] = Key{0, 1}
-	kbd.keys['!'] = Key{0, 0}
+	kbd.keys[0x5f] = Key{0, 5} // backspace; unused, mapped to DEL
+	kbd.keys[0x28] = Key{0, 4}
+	kbd.keys[0x26] = Key{0, 3}
+	kbd.keys[0x25] = Key{0, 2}
+	kbd.keys[0x23] = Key{0, 1}
+	kbd.keys[0x21] = Key{0, 0}
 }
 
 func (kbd *Keyboard) Scan(k Keypress) {
-	var keycode rune
-
-	if k.Char != rune(0) {
-		keycode = k.Char
-	} else {
-		keycode = rune(k.Keycode)
-	}
-
-	key, ok := kbd.keys[keycode]
+	key, ok := kbd.keys[k.Scancode]
 	if ok {
 		kbd.Buffer <- key
 
 		switch k.State {
 		case KEY_DOWN:
 			kbd.matrix.Set(key.row, key.bit)
-			if k.Modifiers.Shift {
-				kbd.matrix.Set(8, 0) // Left shift
+			if k.Shifted {
+				kbd.matrix.Set(8, 5) // Right shift
 			}
 		case KEY_UP:
 			kbd.matrix.Clear(key.row, key.bit)
-			if !k.Modifiers.Shift {
-				kbd.matrix.Clear(8, 0) // Left shift
+			if k.Shifted {
+				kbd.matrix.Clear(8, 5) // Right shift
 			}
 		}
 	}
